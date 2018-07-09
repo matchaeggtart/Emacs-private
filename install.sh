7d6a0eb89109b67e658e53be8172b781fb029866
MatchaEggTart <matchaeggtart@hotmail.com>
2018-07-10 02:50:39 +0800

    sh install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+cp ~/Emacs-private ~/.emacs.d
+rm -rf ~/.emacs.d
+mv .emacs.d ~
+mkdir .emacs.d/site-lisp
+
+cd ~/Downloads
+git clone https://github.com/google/styleguide.git
+mv styleguide google-style
+mv google-style ~/.emacs.d/site-lisp
+
+rm -rf ~/Emacs-private
