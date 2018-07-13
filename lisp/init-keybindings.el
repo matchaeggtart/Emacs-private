9a77f60d18917bda48fa079d760a629e9341ff1b
matchaeggtart <40698156+matchaeggtart@users.noreply.github.com>
2018-07-13 19:07:49 +0800

    Configurations

--- lisp/init-keybindings.el
+++ lisp/init-keybindings.el
@@ -4,4 +4,10 @@
 (global-set-key (kbd "C-h C-v") 'find-variable)
 (global-set-key (kbd "C-h C-k") 'find-function-on-key)
 
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (provide 'init-keybindings)
