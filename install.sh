c458b3e7a477aee52bc5a735a237d9d3ea3be707
matchaeggtart <40698156+matchaeggtart@users.noreply.github.com>
2018-07-19 23:40:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/.emacs.d/site-lisp
-cp -r Emacs-private/* .emacs.d/
-mv Emacs-private/.git/ .emacs.d/
+cp -r Emacs-private/* ~/.emacs.d/
+mv Emacs-private/.git/ ~/.emacs.d/
 
 cd ~/.emacs.d/site-lisp/
 git clone https://github.com/google/styleguide.git
