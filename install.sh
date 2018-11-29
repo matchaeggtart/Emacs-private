73612eadf8036e63837df8c275477ad95825b6a8
Donald Cheung <donald@localhost.localdomain>
2018-11-29 17:29:15 +0800

    sh install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 rm -rf ~/.emacs.d ~/.emacs
-mkdir -p ~/.emacs.d/site-lisp
+mkdir -p ~/.emacs.d/site-lisp/google-style
 cp -r Emacs-private/* ~/.emacs.d/
 mv Emacs-private/.git/ ~/.emacs.d/
 
-cd ~/.emacs.d/site-lisp/
-git clone https://github.com/google/styleguide.git
-mv styleguide google-style
+cd ~/.emacs.d/site-lisp/google-style
+curl -O https://raw.githubusercontent.com/google/styleguide/gh-pages/google-c-style.el
+;; git clone https://github.com/google/styleguide.git
+;; mv styleguide google-style
 
 rm -rf ~/Emacs-private
