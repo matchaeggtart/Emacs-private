b4f7b6d7662f27c8468b285fa3cb45b06aca766f
Donald Cheung <donald@localhost.localdomain>
2018-11-29 17:30:43 +0800

    sh install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,5 @@ mv Emacs-private/.git/ ~/.emacs.d/
 
 cd ~/.emacs.d/site-lisp/google-style
 curl -O https://raw.githubusercontent.com/google/styleguide/gh-pages/google-c-style.el
-;; git clone https://github.com/google/styleguide.git
-;; mv styleguide google-style
 
 rm -rf ~/Emacs-private
