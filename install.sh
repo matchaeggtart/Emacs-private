95ac5b718952e52c3f9e2c46a56c21eec73bfc93
matchaeggtart <40698156+matchaeggtart@users.noreply.github.com>
2018-12-25 13:08:59 +0800

    sh install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 rm -rf ~/.emacs.d ~/.emacs
 mkdir -p ~/.emacs.d/site-lisp/google-style
-cp -r Emacs-private/* ~/.emacs.d/
-mv Emacs-private/.git/ ~/.emacs.d/
+cp -r Emacs-private-old/* ~/.emacs.d/
+mv Emacs-private-old/.git/ ~/.emacs.d/
 
 cd ~/.emacs.d/site-lisp/google-style
 curl -O https://raw.githubusercontent.com/google/styleguide/gh-pages/google-c-style.el
-
-rm -rf ~/Emacs-private
